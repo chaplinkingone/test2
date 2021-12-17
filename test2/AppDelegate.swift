4390d02a84baa04913e791c62fa44c200bc905a7
xiayi <xiayiaxy@icloud.com>
2021-12-17 20:14:14 +0800

    Initial Commit

---
+++ test2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test2
+//
+//  Created by 夏燚 on 2021/12/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 夏燚 on 2021/12/17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
